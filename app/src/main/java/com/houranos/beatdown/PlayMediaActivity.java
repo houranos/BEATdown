7ca778d254db42351ead8e2a05757927afb68d84
Rei Higuchi <rei.higuchi.2001@gmail.com>
2018-03-31 09:48:17 +0900

    Make a basic media player activity. Touch a ListView item to see them.

---
+++ app/src/main/java/com/houranos/beatdown/PlayMediaActivity.java
@@ -0,0 +1,52 @@
+package com.houranos.beatdown;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PlayMediaActivity extends Activity implements SurfaceHolder.Callback {
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+    MediaPlayer mMediaPlayer;
+
+    @Override
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.activity_media_player);
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.stop();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            Intent intent = getIntent();
+            mMediaPlayer = MediaPlayer.create(this, Uri.parse(intent.getStringExtra("FILEPATH")));
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.start();
+        } catch (Exception e) {
+            System.out.println("Opening file failed");
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+}
