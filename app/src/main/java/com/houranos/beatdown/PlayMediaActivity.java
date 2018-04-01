25a3af9b85e2bc68e8b5cf1d048d3fe906c6a80c
Rei Higuchi <rei.higuchi.2001@gmail.com>
2018-04-02 02:15:11 +0900

    Improved appearance of navigation bar

--- app/src/main/java/com/houranos/beatdown/PlayMediaActivity.java
+++ app/src/main/java/com/houranos/beatdown/PlayMediaActivity.java
@@ -19,8 +19,12 @@ public class PlayMediaActivity extends Activity implements SurfaceHolder.Callbac
         super.onCreate(savedInstaceState);
         setContentView(R.layout.activity_media_player);
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         mSurfaceView = (SurfaceView) findViewById(R.id.surface);
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
