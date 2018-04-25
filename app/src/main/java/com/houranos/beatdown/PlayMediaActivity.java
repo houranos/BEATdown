7e90114d75c099596f599254098dab93d02484a4
Rei Higuchi <rei.higuchi.2001@gmail.com>
2018-04-26 01:16:59 +0900

    Implement adjustment of song progress and playback rate.

--- app/src/main/java/com/houranos/beatdown/PlayMediaActivity.java
+++ app/src/main/java/com/houranos/beatdown/PlayMediaActivity.java
@@ -3,16 +3,26 @@ package com.houranos.beatdown;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.media.PlaybackParams;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class PlayMediaActivity extends Activity implements SurfaceHolder.Callback {
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     MediaPlayer mediaPlayer;
+    SeekBar seekBar;
+    SeekBar playbackRate;
+    Handler handler;
+    FloatingActionButton pauseButton;
+    PlaybackParams params;
+
 
     @Override
     public void onCreate(Bundle savedInstaceState) {
@@ -28,6 +38,11 @@ public class PlayMediaActivity extends Activity implements SurfaceHolder.Callbac
         surfaceView = (SurfaceView) findViewById(R.id.surface);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        playbackRate = (SeekBar) findViewById(R.id.playspeed);
+        seekBar.setClickable(false);
+        handler = new Handler();
+//        pauseButton = (Button) findViewById(R.id.)
     }
 
     @Override
@@ -40,14 +55,62 @@ public class PlayMediaActivity extends Activity implements SurfaceHolder.Callbac
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        Intent intent = getIntent();
         try {
-            Intent intent = getIntent();
             mediaPlayer = MediaPlayer.create(this, Uri.parse(intent.getStringExtra("FILEPATH")));
-            mediaPlayer.setDisplay(holder);
-            mediaPlayer.start();
+
         } catch (Exception e) {
             System.out.println("Opening file failed");
         }
+
+        mediaPlayer.setDisplay(holder);
+        mediaPlayer.start();
+        seekBar.setMax(mediaPlayer.getDuration() / 1000);
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer != null) {
+                    int currentPosition = mediaPlayer.getCurrentPosition() / 1000; // In milliseconds
+                    seekBar.setProgress(currentPosition);
+                }
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (mediaPlayer != null && b) {
+                    mediaPlayer.seekTo(i * 1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.start();
+            }
+        });
+        playbackRate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                params = mediaPlayer.getPlaybackParams();
+                params.setSpeed(progress / 100f);
+                mediaPlayer.setPlaybackParams(params);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 
     @Override
